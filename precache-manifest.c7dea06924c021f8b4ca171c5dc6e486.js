b1b2d6bb0c19700eebbc4b21de49039a6ce4011a
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-21 19:08:53 -0500

    Updates

---
+++ precache-manifest.c7dea06924c021f8b4ca171c5dc6e486.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff834444b32a4a61c0eb",
+    "url": "/ReactVideoPlayer/static/js/runtime~main.ff834444.js"
+  },
+  {
+    "revision": "506ccb6f0245f1ca972e",
+    "url": "/ReactVideoPlayer/static/js/main.506ccb6f.chunk.js"
+  },
+  {
+    "revision": "e250b45cfb4563bf5407",
+    "url": "/ReactVideoPlayer/static/js/2.e250b45c.chunk.js"
+  },
+  {
+    "revision": "506ccb6f0245f1ca972e",
+    "url": "/ReactVideoPlayer/static/css/main.7baeb885.chunk.css"
+  },
+  {
+    "revision": "dee9f837887b238e23146632707750fb",
+    "url": "/ReactVideoPlayer/index.html"
+  }
+];
